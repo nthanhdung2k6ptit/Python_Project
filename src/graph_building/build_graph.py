39c958a3e5f05210e5e31cb1fb538395afb85dd3
teanteagame <teanteagame@gmail.com>
2025-11-10 20:43:01 +0700

    fix test graph

--- src/graph_building/build_graph.py
+++ src/graph_building/build_graph.py
@@ -1,6 +1,19 @@
 import pandas as pd
 import networkx as nx
 import json
+import math
+
+def haversine(lat1, lon1, lat2, lon2):
+    lat1, lon1, lat2, lon2 = map(math.radians, [lat1, lon1, lat2, lon2])
+
+    dlat = lat2 - lat1
+    dlon = lon2 - lon1
+
+    a = math.sin(dlat/2)**2 + math.cos(lat1) * math.cos(lat2) * math.sin(dlon/2)**2
+    c = 2 * math.asin(math.sqrt(a))
+
+    R = 6371.0 
+    return R * c
 
 def build_graph(airports_file, routes_file):
     # Load datasets
