416110bf8e4401e91d81e7c6899d5aeb510277f1
teanteagame <teanteagame@gmail.com>
2025-11-11 19:18:17 +0700

    them graph cua vietnam

--- src/graph_building/test_logic.py
+++ src/graph_building/test_logic.py
@@ -7,22 +7,25 @@ from algorithms import shortest_path, shortest_distance, all_paths, graph_metric
 BASE = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", ".."))
 
 # --- Cleaned dataset paths ---
-AIRPORTS_CLEAN = os.path.join(BASE, "data", "cleaned", "airport_db_cleaned.csv")
-ROUTES_CLEAN   = os.path.join(BASE, "data", "cleaned", "routes_cleaned.csv")
+AIRPORTS_WORLD = os.path.join(BASE, "data", "cleaned", "airport_db_cleaned.csv")
+ROUTES_WORLD   = os.path.join(BASE, "data", "cleaned", "routes_cleaned.csv")
+
+AIRPORTS_VN = os.path.join(BASE, "data", "cleaned_vn","airport_db_cleaned_vn.csv")
+ROUTES_VN   = os.path.join(BASE, "data", "cleaned_vn", "routes_cleaned_vn.csv")
 
 # --- Export path ---
 EXPORT_GRAPH = os.path.join(BASE, "data", "graph", "flight_network.json")
-
+EXPORT_GRAPH_VN = os.path.join(BASE, "data", "graph", "flight_netwok_vn.json")
 
 def main():
     print("Building flight network graph...")
 
-    # Build graph based on cleaned and aligned data
-    graph = build_graph(AIRPORTS_CLEAN, ROUTES_CLEAN)
+    graph_world = build_graph(AIRPORTS_WORLD, ROUTES_WORLD)
+    export_graph_json(graph_world, EXPORT_GRAPH)
+    graph_vn = build_graph(AIRPORTS_VN, ROUTES_VN)
+    export_graph_json(graph_vn, EXPORT_GRAPH_VN)
 
-    # Export graph to JSON
-    export_graph_json(graph, EXPORT_GRAPH)
-
+'''
     # Basic summary
     print("Nodes:", graph.number_of_nodes())
     print("Edges:", graph.number_of_edges())
@@ -34,7 +37,7 @@ def main():
     centrality = betweenness_centrality(graph)
     hubs = sorted(centrality.items(), key=lambda x: x[1], reverse=True)[:10]
     print("Top hubs:", hubs)
-
+'''
 
 if __name__ == "__main__":
     main()
